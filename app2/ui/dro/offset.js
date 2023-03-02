84d438c9282c207b1725f61ad3e22f80cfc2244c
Antti Partanen <aehparta@iki.fi>
2023-03-02 11:46:41 +0200

    add fontawesome 5

---
+++ app2/ui/dro/offset.js
@@ -0,0 +1,10 @@
+export default {
+  template: '#tmpl-dro-offset',
+  props: {
+    id: String,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
---
+++ app2/ui/sidebar/sidebar.js
@@ -0,0 +1,7 @@
+export default {
+  template: '#tmpl-sidebar-sidebar',
+  data() {
+    return {};
+  },
+  methods: {},
+};
